defc3c3ca82d72665c0625742f6b2bdec24b7c32
Olep8h <panov@student.vspj.cz>
2022-11-12 20:13:53 +0200

    You can copy any colour from columns by clicking on colour code. + The hash stores information about the colors in the columns

--- project/app.js
+++ project/app.js
@@ -18,32 +18,50 @@ document.addEventListener('click', (event) => {
 
         node.classList.toggle('fa-lock-open')
         node.classList.toggle('fa-lock')
+    } else if (type === 'copy') {
+        copyToClickboard(event.target.textContent)
     }
 })
 
-function generateRandomColor() {
+function gerenerateRandomColor() {
     // RGB
     // #FF0000
-    //#00FF00
-    //0000FF
+    // #00FF00
+    // #0000FF
 
-    const hexCods = '0123456789ABCDEF'
+    const hexCodes = '0123456789ABCDEF'
     let color = ''
     for (let i = 0; i < 6; i++) {
-        color += hexCods[Math.floor(Math.random() * hexCods.length)]
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
     }
     return '#' + color
 }
 
-function setRandomColors() {
-    cols.forEach((col) => {
+function copyToClickboard(text) {
+    return navigator.clipboard.writeText(text)
+}
+
+function setRandomColors(isInitial) {
+    const colors = isInitial ? getColorsFromHash() : []
+
+    cols.forEach((col, index) => {
         const isLocked = col.querySelector('i').classList.contains('fa-lock')
         const text = col.querySelector('h2')
         const button = col.querySelector('button')
-        const color = chroma.random()
 
         if (isLocked) {
+            colors.push(text.textContent)
             return
+        }
+
+        const color = isInitial
+            ? colors[index]
+                ? colors[index]
+                : chroma.random()
+            : chroma.random()
+
+        if (!isInitial) {
+            colors.push(color)
         }
 
         text.textContent = color
@@ -52,6 +70,8 @@ function setRandomColors() {
         setTextColor(text, color)
         setTextColor(button, color)
     })
+
+    updateColorsHash(colors)
 }
 
 function setTextColor(text, color) {
@@ -59,4 +79,22 @@ function setTextColor(text, color) {
     text.style.color = luminance > 0.5 ? 'black' : 'white'
 }
 
-setRandomColors()
+function updateColorsHash(colors = []) {
+    document.location.hash = colors
+        .map((col) => {
+            return col.toString().substring(1)
+        })
+        .join('-')
+}
+
+function getColorsFromHash() {
+    if (document.location.hash.length > 1) {
+        return document.location.hash
+            .substring(1)
+            .split('-')
+            .map((color) => '#' + color)
+    }
+    return []
+}
+
+setRandomColors(true)
