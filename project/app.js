487953717cc4e2e9039f264919cc4c98cfdbd0d0
Olep8h <panov@student.vspj.cz>
2022-11-12 19:28:01 +0200

    castle animation change

--- project/app.js
+++ project/app.js
@@ -11,7 +11,13 @@ document.addEventListener('click', (event) => {
     const type = event.target.dataset.type
 
     if (type === 'lock') {
-        console.log('perform lock');
+        const node =
+            event.target.tagName.toLowerCase() === 'i'
+                ? event.target
+                : event.target.children[0]
+
+        node.classList.toggle('fa-lock-open')
+        node.classList.toggle('fa-lock')
     }
 })
 
